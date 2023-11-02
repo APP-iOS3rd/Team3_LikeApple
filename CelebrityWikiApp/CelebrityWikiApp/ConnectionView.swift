c1e3f1f7f918be1e360b2eff1679494350d6a28f
NohJooYoung <nohjoo97@naver.com>
2023-11-02 20:38:04 +0900

    탭 뷰 first commit

---
+++ CelebrityWikiApp/CelebrityWikiApp/ConnectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionView.swift
+//  CelebrityWikiApp
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ConnectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectionView()
+}
