7b5fa16e0fb667722a9a83a34386b8ff05f63591
Seobe <woodyms@nate.com>
2023-11-02 14:51:43 +0900

    셀럽 위키 앱 프로젝트  추가

---
+++ CelebrityWikiApp/CelebrityWikiApp/CelebrityWikiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelebrityWikiAppApp.swift
+//  CelebrityWikiApp
+//
+//  Created by seobe22 on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CelebrityWikiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CelebrityWikiApp/CelebrityWikiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CelebrityWikiApp
+//
+//  Created by seobe22 on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
