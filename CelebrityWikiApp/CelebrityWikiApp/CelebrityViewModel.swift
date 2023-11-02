f6123b2aaff42781d1d3bf01b7c084f41fec5bf5
NohJooYoung <nohjoo97@naver.com>
2023-11-02 21:15:09 +0900

    [edit] 뷰 모델 분리

---
+++ CelebrityWikiApp/CelebrityWikiApp/CelebrityViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CelebrityViewModel.swift
+//  CelebrityWikiApp
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import Foundation
+
+class CelebrityViewModel: ObservableObject {
+    
+    
+    //Json 파싱
+    func loadJson<T: Decodable>(_ filename: String) -> T {
+        let data: Data
+
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("\(filename) not found.")
+        }
+
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Could not load \(filename): \(error)")
+        }
+
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            fatalError("Unable to parse \(filename): \(error)")
+        }
+
+    }
+}
