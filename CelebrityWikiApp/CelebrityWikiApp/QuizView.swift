ba82760fa0b4bc8072613dbd82ac52372506994a
NohJooYoung <nohjoo97@naver.com>
2023-11-02 15:27:50 +0900

    퀴즈 화면 first commit

---
+++ CelebrityWikiApp/CelebrityWikiApp/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  CelebrityWikiApp
+//
+//  Created by 노주영 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
